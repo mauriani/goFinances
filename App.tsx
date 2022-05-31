63f9f986b8b160e92d4c2a866df176a336a7ab9a
mauriani <maurianim@gmail.com>
2022-05-31 15:23:41 -0300

    finalizado

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import "react-native-gesture-handler";
 import "intl";
 import "intl/locale-data/jsonp/pt-BR";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { ThemeProvider } from "styled-components";
+import * as Font from "expo-font";
 import { Routes } from "./src/routes";
 
 import {
-  useFonts,
   Poppins_500Medium,
   Poppins_400Regular,
   Poppins_700Bold,
@@ -20,38 +20,48 @@ import { AuthProvider, useAuth } from "./src/hooks/auth";
 import theme from "./src/global/styles/theme";
 
 export default function App() {
-  // vamos usar isso para saber se as fonts foram ou nao carregadas
-  let [fontsLoaded] = useFonts({
-    Poppins_500Medium,
-    Poppins_400Regular,
-    Poppins_700Bold,
-  });
-
+  const [fontsLoaded, setFontsLoaded] = useState(false);
   const { userStorageLoading } = useAuth();
+
+  const useFonts = async () => {
+    await Font.loadAsync({
+      medium: Poppins_500Medium,
+      regular: Poppins_400Regular,
+      bold: Poppins_700Bold,
+    });
+  };
 
   async function load() {
     if (!fontsLoaded || userStorageLoading) {
       await SplashScreen.preventAutoHideAsync();
       await new Promise((resolve) => setTimeout(resolve, 2000));
+      setFontsLoaded(true);
     } else {
       await SplashScreen.hideAsync();
     }
   }
 
+  const LoadFonts = async () => {
+    await useFonts(); // Here we will have to await the call
+  };
+
   useEffect(() => {
     load();
+    LoadFonts();
   }, [fontsLoaded, userStorageLoading]);
 
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <StatusBar
         translucent
         backgroundColor="transparent"
         barStyle="light-content"
       />
-      <AuthProvider>
-        <Routes />
-      </AuthProvider>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </ThemeProvider>
+    </>
   );
 }
